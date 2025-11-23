35b8c80b63f94b52cd3946e7091c8f0ca08f1269
wo-o29 <woogur29@gmail.com>
2025-11-23 18:44:21 +0900

    feat: react-query 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { GlobalPortal, GlobalStyles } from 'tosslib';
 import { Routes } from './pages/Routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export function App() {
   return (
     <>
       <GlobalStyles />
-      <GlobalPortal.Provider>
-        <Routes />
-      </GlobalPortal.Provider>
+      <QueryClientProvider client={queryClient}>
+        <GlobalPortal.Provider>
+          <Routes />
+        </GlobalPortal.Provider>
+      </QueryClientProvider>
     </>
   );
 }
